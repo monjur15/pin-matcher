a5c3d59dff62951b3cdaed4bd629ffbb47d6d3b0
monjur15 <monjur.git@gmail.com>
2021-08-25 23:01:16 +0600

    Pin generate done

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+
+        return pin;
+
+    }
+    else {
+        // console.log('got 3 digit', pin);
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
