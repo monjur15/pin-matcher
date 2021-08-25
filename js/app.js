8201fbac768f0a36665dd7878ad7401437226451
monjur15 <monjur.git@gmail.com>
2021-08-25 23:15:24 +0600

    updated semicolon

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@ function getPin() {
         // console.log('got 3 digit', pin);
         return getPin();
     }
-}
+};
 
 function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
-}
+};
 
